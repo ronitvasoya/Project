c3f41f613617341dc8a2bc92e12dd9e8c6181160
Abhishek3031 <patelabhishek301@gmail.com>
2018-11-19 11:18:39 +0530

    Add one button

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btnclick(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
